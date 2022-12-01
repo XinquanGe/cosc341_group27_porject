2cd7d38487e48505dfca46b950afe8bce9508861
lhenon999 <leohenon@gmail.com>
2022-11-30 18:55:19 -0800

    added map in information page

--- app/src/main/java/com/example/cosc341_porject/InformationActivity.java
+++ app/src/main/java/com/example/cosc341_porject/InformationActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.FileInputStream;
@@ -94,6 +95,18 @@ public class InformationActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        ImageView mapBtn = findViewById(R.id.mapBtn);
+
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InformationActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
 
     }
